20d0ce919045f3e4cc001979434382e784f622ad
Susiane Rodrigues <susiane@folhacerta.com>
2017-10-06 22:33:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/susi/swipewithanimation/SwipeButton.java
@@ -0,0 +1,54 @@
+package com.example.susi.swipewithanimation;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Susi on 10/6/17.
+ */
+
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+public class SwipeButton extends RelativeLayout {
+
+    public SwipeButton(Context context) {
+        super(context);
+        init(context, null, -1, -1);
+    }
+
+    public SwipeButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, -1, -1);
+    }
+
+    public SwipeButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr, -1);
+    }
+
+    @TargetApi(21)
+    public SwipeButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+
+        RelativeLayout background = new RelativeLayout(context);
+
+        LayoutParams layoutParamsView = new LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        layoutParamsView.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
+        background.setBackground(ContextCompat.getDrawable(context, R.drawable.shape_rounded));
+
+        addView(background, layoutParamsView);
+    }
+}
